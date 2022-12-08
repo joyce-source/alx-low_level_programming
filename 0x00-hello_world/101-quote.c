3bde3f411b7b271e5c1dd16da28129dfd9d17f49
joyce-source <brownjojo96@gmail.com>
2022-12-08 06:37:47 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,12 @@
 /**
  * main - prints exactly and that piece of art is useful
  *
- * Return 1
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 
 }

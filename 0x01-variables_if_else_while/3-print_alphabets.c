6334829d7258820b84e1ba041e7685f248cb64fb
joyce-source <brownjojo96@gmail.com>
2022-12-09 04:22:28 -0800

    lower_and_uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ *
+ * Return: Alway o (Success)
+ */
+int main(void)
+{
+	char x;
+
+	char y;
+
+	x = 'a';
+	y = 'A';
+	while
+		(x <= 'z') {
+			putchar(x);
+			x++;
+		}
+	while
+		(y <= 'z') {
+			putchar(y);
+			y++;
+		}
+	putchar('\n');
+	return (0);
+}

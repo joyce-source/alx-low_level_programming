200c949a0fa47ca857b494bf54d7e9cbb5551484
joyce-source <brownjojo96@gmail.com>
2022-12-09 08:49:21 -0800

    3-print

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main-entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char r
+	r = 'a';
+	while
+		(r <= 'z') {
+			if ((r != 'e') && r <= 'z')
+				putchar(r);
+			r++;
+		}
+	putchar('\n');
+	return (0);
+}

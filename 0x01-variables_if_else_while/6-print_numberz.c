b5c94aeb16fd1ba09ab37d29df4c622c863f3ed7
joyce-source <brownjojo96@gmail.com>
2022-12-09 09:57:24 -0800

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main-entry point
+ *
+ * return:Always 0 (sucess)
+ */
+int main(void)
+{
+	int ga;
+
+	ga = 0;
+	while
+		(ga < 10) {
+			putchar(ga + '0');
+			ga++;
+		}
+	putchar('\n');
+	return (0);
+}

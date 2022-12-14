850636f108dd031210a4975a00b32a74ef76e7e2
joyce-source <brownjojo96@gmail.com>
2022-12-13 23:15:41 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char q = 'a';
+	while (q <= 'z')
+	{
+		_putchar(q );
+		q++;
+	}
+
+	_putchar('\n');
+}

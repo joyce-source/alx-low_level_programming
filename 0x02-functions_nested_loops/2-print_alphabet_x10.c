61832914600fa94d469e01db292ca90720a9160f
joyce-source <brownjojo96@gmail.com>
2022-12-13 23:50:38 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -Entry point
+ *
+ * Return 0
+ */
+void print_alphabet_x10(void)
+{
+char w = 0, j;
+
+	while (w <= 10)
+	{
+		j = 'a'
+		while (j <= 'z')
+
+		{
+		_putchar(j);
+		j++;
+		}
+		_putchar('\n');
+		w++;
+	}
+
+	return (0);
+}
+

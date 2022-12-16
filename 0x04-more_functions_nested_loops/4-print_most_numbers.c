4247b97044824d55d6d6ab060b72b03c499c9d7c
joyce-source <brownjojo96@gmail.com>
2022-12-16 10:01:32 -0800

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,10 +8,10 @@ void print_most_numbers(void)
 {
 	int ch;
 
-	for (ch = 48; ch < 58; ch++)
+	for (ch = 0; ch < 10; ch++)
 	{
-		if (ch != 50 && ch != 52)
-			_putchar(ch);
+		if (ch != 2 && ch != 4)
+			_putchar(ch + '0');
 	}
 
 	_putchar('\n');

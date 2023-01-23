c1853fd891e8c4a4ff211a23a2a49635092e1bc3
joyce-source <brownjojo96@gmail.com>
2023-01-23 06:34:43 -0800

    mainerror

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,7 +2,7 @@
 #define VARIADIC_H_
 
 int_putchar(char c);
-int sum _them_all(const unsingned int n, ...);
+int sum_them_all(const unsingned int n, ...);
 void print_number(const char *separator,const unsigned int n,...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char *const format, ...);

6b8aa2d2c3cbd90f177f124777bdb9a15a66eff3
joyce-source <brownjojo96@gmail.com>
2023-01-23 06:42:04 -0800

    task0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,8 @@
 #ifndef VARIADIC_H_
-#define VARIADIC_H_
+#define VARIADI_H_
 
-int_putchar(char c);
-int sum_them_all(const unsingned int n, ...);
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
 void print_number(const char *separator,const unsigned int n,...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char *const format, ...);

ad4fd36520295e0a1419145ad7e10cf1a71b8f57
joyce-source <brownjojo97@gmail.com>
2023-03-24 04:08:58 -0700

    update

--- 0x18-dynamic_libraries/sub.c
+++ 0x18-dynamic_libraries/sub.c
@@ -1,7 +1,16 @@
-int add(int a, int b) {
-    return a + b;
+#include <stdio.h>
+/**
+  *int add - function that adds two integers
+  *Return - value
+  *void greet - function that prints a greeting message for the given name
+  *Retun -Always (0)
+  */
+int add(int a, int b)
+{
+	return (a + b);
 }
 
-int subtract(int a, int b) {
-    return a - b;
+void greet(char *name)
+{
+	printf("Hello, %s!\n", name);
 }

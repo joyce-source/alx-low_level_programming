683cc067aa07df2f711747e4481023f1b28eddf7
joyce-source <jojobrown97@gmail.com>
2023-04-12 08:44:15 +0000

    task_errors

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 
 /**
-* where size is the size of the array
-* Returns a pointer to the newly created hash table
+* hash_table_create - function creates new hash table with an array of nodes.
+* Return: a pointer to the created hash table
+* @size: is the size of the array*
 */
 
 hash_table_t *hash_table_create(unsigned long int size)
@@ -11,14 +12,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 hash_table_t *table = malloc(sizeof(hash_table_t));
 if (table == NULL)
 {
-return NULL;
+return (NULL);
 }
 table->size = size;
 table->array = calloc(size, sizeof(hash_table_t));
 if (table->array == NULL)
 {
 free(table);
-return NULL; /*error: could not allocate memory for the items array*/
+return (NULL); /*error: could not allocate memory for the items array*/
 }
-return table;
+return (table);
 }
